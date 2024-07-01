2487f22636421b3f9fdf601e955dc03de7e59daa
Derricknjaramba <njarambaderrick@gmail.com>
2024-07-01 10:43:08 +0300

    arithimetic lab

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
+function multiply(num-1,num-2){
 
-// Write your code here
+    let c=num-1*num-2
+}
+multiply(31,2);
+
+function random()
+[
+    let y=Math.floor(Math.random()*10) +1;
+]
+random();
+
+function mod_(x,y){
+    let b=x%y;
+
+}
+mod_(9,5);
+
+function max(){
+    let v=[3,4,6,8,14,7,20];
+    let m=Math.max(v);
+}
+max();
